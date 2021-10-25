80d30de8927b19ee9fadf40bfb5a1385a774030c
Liang Wang <wang.leon@gmail.com>
2021-10-25 23:59:01 +0800

    add Aperture class

---
+++ teilice/aperture.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+class Aperture(object):
+    def __init__(self, name):
+        self.name = name
+        if name=='3x3':
+            self.shape = (3, 3)
+            self.data = np.array([
+                [1, 1, 1],
+                [1, 1, 1],
+                [1, 1, 1],
+                ])
+            self.center = (1, 1)
+        elif name=='5x5':
+            self.shape = (5, 5)
+            self.data = np.array([
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                ])
+            self.center = (2, 2)
+        elif name=='5x5-4':
+            self.shape = (5, 5)
+            self.data = np.array([
+                [0, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [0, 1, 1, 1, 0],
+                ])
+            self.center = (2, 2)
