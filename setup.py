1900e8a83aa5ba580f49eef07670f2435df6244d
Liang Wang <wang.leon@gmail.com>
2026-03-14 15:06:13 +0800

    update version number to 0.3

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 
 setup(
     name         = 'teilice',
-    version      = '0.2',
+    version      = '0.3',
     description  = 'TESS Full Frame Image Photometry Package',
     author       = 'Liang Wang',
     author_email = 'wang.leon@gmail.com',
     license      = 'Apache-2.0',
     packages     = [
                     'teilice',
+                    'teilice/tools',
                     ],
 
     )
